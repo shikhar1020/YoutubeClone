060b09cc905dc9266c8e2c73063a271b8b4dcfef
Shikhar <shikhar.17.srivastava@st.niituniversity.in>
2020-09-14 20:54:39 +0530

    Initialized React App

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          About to build a Youtube Clone!! Yehheye!!
         </a>
       </header>
     </div>
